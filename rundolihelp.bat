2da764a4a901630d7fc6d4dc92af276b0987e6cf
Ezzoubairfarkhi <zfarkhi11@gmail.com>
2025-04-29 14:01:25 +0100

    Add files via upload

---
+++ rundolihelp.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Launch Dolibarr Help center
+REM ---------------------------
+
+REM Go to help center page
+echo Please wait...
+start "iexplore.exe" http://localhost:81/dolibarr/support/
